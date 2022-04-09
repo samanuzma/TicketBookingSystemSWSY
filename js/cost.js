dc04a8541fe5b34427f601aa929bab5c581b0811
Saman Uzma <saman.uzma@gmail.com>
2022-04-09 10:22:41 +1000

    Comments added to cost.js

---
+++ js/cost.js
@@ -0,0 +1,7 @@
+// How to fetch Id and Classes from HTML
+
+// let adultid = (id) => document.getElementById(adults);
+// let classes = (classes) => document.getElementsByClassName(classes);
+
+// Use id.value to fetch the value inside the fields
+// let adult_number = adultid.value;
